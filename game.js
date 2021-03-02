957dc20feea4c9abc3d0e10ddec9c7defef0afcc
Mohd. Ayan Ansari <objectorienteddev@gmail.com>
2021-03-02 21:56:31 +0530

    origin

---
+++ game.js
@@ -0,0 +1,90 @@
+var userClickedPattern = [];
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+//-------------------------------------------------------
+//keypress
+var started = false;
+var level = 0;
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level" + level);
+    nextSequence();
+    started = true;
+  }
+});
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+//===============================
+//game logic
+//===============================
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    //add class on game over for red background
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    //change title to game over...
+
+    startOver();
+  }
+}
+//===============================
+//game logic end---------------
+//===============================
+//generate no. bet 1-4
+function nextSequence() {
+  userClickedPattern = [];
+  //level increses each time called nextSequence() function.
+  level++;
+
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+//play sound on click
+function playSound(name) {
+  var audio = new Audio("/sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//animation
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+//==================================
+//Restarting The Geme
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+//Restarting game code ends
+//==================================
+//--------------------------------------------------------
